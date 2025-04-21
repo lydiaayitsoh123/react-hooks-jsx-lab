c58c7fbed955e207fa1697cbb008e10374099bb6
LYDIA <lydiaayitsoh@gmail.com>
2025-04-21 09:53:00 +0300

    added commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { image } from "../data/data";
 //import About from "../components/About";
-import Image from "../assets/myImage.jpg"; // ✅ use the actual file path
+//import Image from "../assets/myImage.jpg"; // ✅ use the actual file path
 
 
 function About() {
-  return (<div id="About">
+  return (<div id="about">
     <h2>About Me</h2>
     <p>Am Strong</p>
     <img src={image} alt="I made this" />
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,11 +3,13 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return( <div id="Home">
-   <h1 style={{ color: "firebrick" }}>
-        Lydia is a Web Developer from Nairobi
+  return (
+    <div id="home">
+      <h1 style={{ color: 'firebrick' }}>
+        {name} is a Web Developer from {city}
       </h1>
-  </div>);
+    </div>
+  );
 }
 
 export default Home;
